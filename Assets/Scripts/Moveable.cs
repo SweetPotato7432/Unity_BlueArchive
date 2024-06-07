0caa76abaf765bdbb94aae4169996746f2f35fce
SP_GoGuMa <wl7432@gmail.com>
2024-06-07 19:15:39 +0900

    nevmesh test
    nevmesh test

---
+++ Assets/Scripts/Moveable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+
+    [SerializeField]
+    Transform target;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space)) 
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
