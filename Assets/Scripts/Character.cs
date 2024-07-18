f746756e8cdda3ad7c08111bf56935dca48f1209
SP_GoGuMa <wl7432@gmail.com>
2024-07-18 18:07:04 +0900

    Revert "Update Character.cs"
    This reverts commit c2d4bd4f2dcd28ee960f04ac780aaa573419f37a.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -314,6 +314,10 @@ public class Character : MonoBehaviour
             // 회피
             //Debug.Log($"{stat.Name}가 {attakerStat.Name}의 공격에 회피");
         }
+        
+        
+
+        
     }
 
     private void Reload()
