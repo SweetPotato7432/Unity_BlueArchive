0e8f92780b434492d81669c9df96ca350a814666
SP_GoGuMa <wl7432@gmail.com>
2024-09-25 21:54:53 +0900

    재장전 UI 구현, 폭팔타입 캐릭터 변경
    재장전 UI 구현 완료
    
    폭발타입 캐릭터 아즈사가 리깅이 안되어있어, 리깅 되어 있는 카요코로 변경
    
    추후 계획
    - 캐릭터 애니메이션 적용

--- Assets/Scripts/UI/ReloadUI.cs
+++ Assets/Scripts/UI/ReloadUI.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReloadUI : MonoBehaviour
 {
+    private string parentTag;
+
+    private Vector3 movePos;
+
+    [SerializeField]
+    private GameObject reloadBG;
+
     // Start is called before the first frame update
+    void Awake()
+    {
+        parentTag = transform.parent.tag;
+        if (parentTag == "Enemy")
+        {
+            movePos = new Vector3(-1.5f, 0, 0);
+            reloadBG.transform.localScale = new Vector3(-reloadBG.transform.localScale.x,reloadBG.transform.localScale.y,reloadBG.transform.localScale.z);
+        }
+        else if (parentTag == "Ally")
+        {
+            movePos = new Vector3(1.5f, 0, 0);
+        }
+    }
+
     void OnEnable()
     {
-        Vector3 targetPos = transform.parent.transform.position - new Vector3(1.5f, 0, 0);
+        Debug.Log(movePos);
+        Vector3 targetPos = transform.parent.transform.position - movePos;
 
         targetPos.y += 1;
         transform.position = targetPos;
